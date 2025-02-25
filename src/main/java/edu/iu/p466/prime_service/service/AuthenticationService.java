5389bfc94c93786ad8e2a320370b2e1d61fd9438
Tanner Novak <tannovak@iu.edu>
2025-02-25 16:02:16 -0500

    practicum 2

---
+++ src/main/java/edu/iu/p466/prime_service/service/AuthenticationService.java
@@ -0,0 +1,62 @@
+package edu.iu.p466.prime_service.service;
+
+
+import edu.iu.p466.prime_service.model.Customer;
+import edu.iu.p466.prime_service.repository.AuthenticationFileRepository;
+import edu.iu.p466.prime_service.repository.IAuthenticationFileRepository;
+import edu.iu.p466.prime_service.service.IAuthenticationService;
+import edu.iu.p466.prime_service.service.*;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Service
+public class AuthenticationService
+        implements IAuthenticationService , UserDetailsService {
+
+    IAuthenticationFileRepository  authenticationRepository;
+
+
+    public AuthenticationService(IAuthenticationFileRepository  authenticationRepositroy){
+        this.authenticationRepository = authenticationRepositroy;
+    }
+
+    @Override
+    public boolean register(Customer customer) throws IOException {
+        BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+        String passwordEncoded = bc.encode(customer.getPassword());
+        customer.setPassword(passwordEncoded);
+        return authenticationRepository.save(customer);
+    }
+
+
+
+    @Override
+    public boolean login(String username, String password) throws IOException {
+        return false;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            Customer customer = authenticationRepository.findByUserName(username);
+            if(customer == null){
+                throw new UsernameNotFoundException("");
+            }
+            return User
+                    .withUsername(username)
+                    .password(customer.getPassword())
+                    .build();
+
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
